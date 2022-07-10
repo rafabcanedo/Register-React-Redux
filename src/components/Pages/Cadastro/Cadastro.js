56de46c6da604e86f0aec94539d8bf6851ca80b3
rafabcanedo <rafaitecanedo@gmail.com>
2022-07-09 23:46:24 -0300

    up register page

--- src/components/Pages/Cadastro/Cadastro.js
+++ src/components/Pages/Cadastro/Cadastro.js
@@ -1,40 +1,50 @@
 import React from 'react';
 import './Cadastro.css';
 
+import { Link } from 'react-router-dom';
+
 const Cadastro = () => {
  return(
   <div className='section'>
     <form className='cadastro'>
+    
     <h1 className='title'>Bem Vindo</h1>
 
+    <div className='form-container'>
     <div className='wrap-input'>
-      <label>Nome Completo</label>
-      <input type="text" />
+      <label>Nome Completo </label>
+      <input className='input-camp' type="text" />
     </div>
 
-    <div className='wrap-input'>
-      <label>Email</label>
-      <input type="email" />
+     <div className='wrap-input'>
+      <label>Email </label>
+      <input className='input-camp' type="email" />
+     </div>
+
+     <div className='wrap-input'>
+      <label>Senha </label>
+      <input className='input-camp' type="password" />
+     </div>
+
+     <div className='wrap-input'>
+      <label>Data de nascimento </label>
+      <input className='input-camp' type="date" />
+     </div>
+
+     <div className='wrap-input'>
+      <label>CPF </label>
+      <input className='input-camp' type="number" />
+     </div>
     </div>
 
-    <div className='wrap-input'>
-      <label>Senha</label>
-      <input type="password" />
-    </div>
-
-    <div className='wrap-input'>
-      <label>Data de nascimento</label>
-      <input type="date" />
-    </div>
-
-    <div className='wrap-input'>
-      <label>CPF</label>
-      <input type="number" />
-    </div>
 
     <div className='button-submit'>
-      <input type="submit" value="Cadastrar" />
+      <Link to='/home'>
+      <button className='form-button'>Cadastrar</button>
+      </Link>
     </div>
+
+    
     </form>
   </div>
  )
@@ -42,10 +52,6 @@ const Cadastro = () => {
 
 export default Cadastro;
 
-/*
-Nome Completo
-CPF
-Email
-Data de Nascimento
-Senha
-*/
+// o form-container ficou muito centralizado, talvez seja bom mudar...
+
+// <input type="submit" value="Cadastrar" />
